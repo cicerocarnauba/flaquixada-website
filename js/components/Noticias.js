b873355486fef851d979a06aca2fe69eec6a2655
higor <higormc2015@gmail.com>
2026-06-13 14:57:51 -0300

    feat: card noticias && refactor: corrigindo estrutura de layout && fix: pequenos ajustes de design

---
+++ js/components/Noticias.js
@@ -0,0 +1,56 @@
+const dadosNoticias = [
+    {
+        id: 1,
+        titulo: 'Grande evento no Consulado neste sábado',
+        resumo: 'Venha assistir ao jogo com a melhor torcida de Quixadá!',
+        link: '#'
+    },
+    {
+        id: 2,
+        titulo: 'Grande evento no Consulado neste sábado',
+        resumo: 'Venha assistir ao jogo com a melhor torcida de Quixadá!',
+        link: '#'
+    },
+    {
+        id: 3,
+        titulo: 'Grande evento no Consulado neste sábado',
+        resumo: 'Venha assistir ao jogo com a melhor torcida de Quixadá!',
+        link: '#'
+    }
+];
+
+class Noticias extends HTMLElement {
+    async connectedCallback() {
+        try {
+            const resposta = await fetch('./components_html/noticias.html');
+            const htmlPuro = await resposta.text();
+
+            this.innerHTML = `
+                <link rel="stylesheet" href="./css/components/noticias.css">
+                ${htmlPuro}
+            `;
+            
+            this.renderizarNoticias();
+        } catch (error) {
+            console.error('Erro ao carregar o HTML do card de notícias:', error);
+        }
+    }
+
+    renderizarNoticias() {
+        const container = this.querySelector('.noticias-list');
+        if (!container) return;
+
+        dadosNoticias.forEach(noticia => {
+            const item = document.createElement('div');
+            item.className = 'noticia-item';
+            
+            item.innerHTML = `
+                <h3 class="noticia-titulo"><a href="${noticia.link}">${noticia.titulo}</a></h3>
+                <p class="noticia-resumo">${noticia.resumo}</p>
+            `;
+            container.appendChild(item);
+        });
+    }
+}
+
+customElements.define('noticias-fla', Noticias);
